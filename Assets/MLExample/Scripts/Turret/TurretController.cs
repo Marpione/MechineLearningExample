fcb5cb76adb885e5456f0a4da3f9eed3d8f8d401
Marpione <34212966+Marpione@users.noreply.github.com>
2020-03-02 19:38:58 +0300

    Turret added

---
+++ Assets/MLExample/Scripts/Turret/TurretController.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TurretController : MonoBehaviour
+{
+    public Transform PartToRotate;
+
+    public float RotateSpeed;
+    public float FireRate;
+}
